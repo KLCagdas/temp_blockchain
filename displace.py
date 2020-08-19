9f6d9ebf74443c86eb4298e6ea7647747a004a73
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2020-08-19 17:01:22 +0300

    Update displace.py
    Displacement is fixed and adjusted in respect to neighborhood relations. All the atoms are responsible only for the displacement between them and their neighbors.

--- displace.py
+++ displace.py
@@ -1,9 +1,16 @@
 import numpy as np
 from random import uniform
+def displace(block_chain, block_chain0, N, neighborhood):
+    rand_displace = []
+    displacement = []
 
-def displace(block_chain, block_chain0, N, rand_displace):
-    for i in range(N**2):
+    for i in range(N**2):       # consider getting rid of this with a random function for the whole list
         rand_displace.append([uniform(-0.5, 0.5), uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
     rand_displace = np.array(rand_displace)
     block_chain = np.add(block_chain, rand_displace)
-    return np.subtract(block_chain, block_chain0)
+
+    for atom, neighbor in neighborhood:       # index of the i-th atom and its neighbor
+        initial_distance = np.subtract(block_chain0[atom], block_chain0[neighbor])
+        instant_distance = np.subtract(block_chain[atom], block_chain[neighbor])
+        displacement.append(list(np.subtract(instant_distance, initial_distance)))
+    return np.array(displacement)
