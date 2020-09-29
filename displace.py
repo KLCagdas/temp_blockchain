708b9a8f8e6221904eef2b54fb93497027e07b48
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2020-09-29 10:53:37 +0300

    Update displace.py

--- displace.py
+++ displace.py
@@ -1,16 +1,25 @@
 import numpy as np
-from random import uniform
-def displace(block_chain, block_chain0, N, neighborhood):
-    rand_displace = []
-    displacement = []
-
-    for i in range(N**2):       # consider getting rid of this with a random function for the whole list
-        rand_displace.append([uniform(-0.5, 0.5), uniform(-0.5, 0.5), uniform(-0.5, 0.5)])
-    rand_displace = np.array(rand_displace)
-    block_chain = np.add(block_chain, rand_displace)
-
-    for atom, neighbor in neighborhood:       # index of the i-th atom and its neighbor
-        initial_distance = np.subtract(block_chain0[atom], block_chain0[neighbor])
-        instant_distance = np.subtract(block_chain[atom], block_chain[neighbor])
-        displacement.append(list(np.subtract(instant_distance, initial_distance)))
-    return np.array(displacement)
+def displace(neighbortable, x, y, z, x0, y0, z0, boundary, block_chain):
+    displacement = np.zeros(np.shape(block_chain))
+    for atom, neighborhood in enumerate(neighbortable):     # recall that neighbor follows the pattern "yxxy"
+        if not all(boundary[atom]):     # add an extra for loop for neighbor index
+            displacement[atom] = [0, 0, z0[0]]
+        else:    # if atom is not on the boundary 
+            for neighbor in neighborhood:
+                dx1 = (x[neighbor] - x[atom]) - (x0[neighbor] - x0[atom])
+                dx2 = (x[neighbor] - x[atom]) - (x0[neighbor] - x0[atom])
+                dx3 = (x[neighbor] - x[atom]) - (x0[neighbor] - x0[atom])
+                dx4 = (x[neighbor] - x[atom]) - (x0[neighbor] - x0[atom])
+                dx = dx1 + dx2 + dx3 + dx4
+                dy1 = (y[neighbor] - y[atom]) - (y0[neighbor] - y0[atom])
+                dy2 = (y[neighbor] - y[atom]) - (y0[neighbor] - y0[atom])
+                dy3 = (y[neighbor] - y[atom]) - (y0[neighbor] - y0[atom])
+                dy4 = (y[neighbor] - y[atom]) - (y0[neighbor] - y0[atom])
+                dy = dy1 + dy2 + dy3 + dy4
+                dz1 = (z[neighbor] - z[atom]) - (z0[neighbor] - z0[atom])
+                dz2 = (z[neighbor] - z[atom]) - (z0[neighbor] - z0[atom])
+                dz3 = (z[neighbor] - z[atom]) - (z0[neighbor] - z0[atom])
+                dz4 = (z[neighbor] - z[atom]) - (z0[neighbor] - z0[atom])
+                dz = dz1 + dz2 + dz3 + dz4
+                displacement[atom] = [dx, dy, dz]
+    return displacement
